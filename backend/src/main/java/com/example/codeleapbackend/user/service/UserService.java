abaa60d57528ed70a3a10013af2b3698172540e5
Icsya05 <2797066299@qq.com>
2026-09-08 21:28:24 +0800

    feat(auth): update backend/src/main/java/com/example/codeleapbackend/user/service/UserService.java

--- backend/src/main/java/com/example/codeleapbackend/user/service/UserService.java
+++ backend/src/main/java/com/example/codeleapbackend/user/service/UserService.java
@@ -3,9 +3,9 @@ package com.example.codeleapbackend.user.service;
 import com.example.codeleapbackend.user.pojo.dto.LoginParam;
 import com.example.codeleapbackend.user.pojo.entity.User;
 
-
 public interface UserService {
-    public void register(LoginParam loginParam);
-    public User login(LoginParam loginParam);
-    public void resetPassword(LoginParam loginParam);
+    void register(LoginParam loginParam);
+    User login(LoginParam loginParam);
+    User getById(Long id);
+    void changePassword(Long userId, String oldPassword, String newPassword);
 }
